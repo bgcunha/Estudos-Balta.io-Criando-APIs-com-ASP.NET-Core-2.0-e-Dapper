634fdda1bb10a520cbfed9ee6d7d1ebfc7b2bd17
Bruno Expedito Gomes da Cunha <bruno.gomes.da.cunha@avanade.com>
2022-01-28 17:02:03 -0300

    Ajuste de collections

--- BaltaStore.Domain/StoreContext/Entities/Customer.cs
+++ BaltaStore.Domain/StoreContext/Entities/Customer.cs
@@ -1,25 +1,36 @@
 using BaltaStore.Domain.StoreContext.ValueObjects;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BaltaStore.Domain.StoreContext.Entities
 {
     public class Customer
     {
+        private readonly IList<Address> _addresses;
+
         public Customer(Name name, Document document, Email email, string phone)
         {
             Name = name;
             Document = document;
             Email = email;
             Phone = phone;
-            Addresses = new List<Address>();
+            _addresses = new List<Address>();
         }
 
         public Name Name { get; private set; }
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Addresses { get; private set; }
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
 
+        public void AddAddress(Address address)
+        {
+            //TODO - Validar Address
+            //TODO - Adicionar Address
+            _addresses.Add(address);
+
+
+        }
         public override string ToString()
         {
             return Name.ToString();
