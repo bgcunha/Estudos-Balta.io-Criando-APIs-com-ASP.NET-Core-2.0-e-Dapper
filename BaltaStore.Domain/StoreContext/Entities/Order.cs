646e97d13adbcf0df7790551ea75988966758434
Bruno Gomes <brunnu.cunha@gmail.com>
2022-01-31 14:48:59 -0300

    Simulando o pedido

--- BaltaStore.Domain/StoreContext/Entities/Order.cs
+++ BaltaStore.Domain/StoreContext/Entities/Order.cs
@@ -51,7 +51,7 @@ namespace BaltaStore.Domain.StoreContext.Entities
         {
             // A cada 5 produtos é uma entrega
             var deliveries = new List<Delivery>();
-            // deliveries.Add(new Delivery(DateTime.Now.AddDays(5)));
+            deliveries.Add(new Delivery(DateTime.Now.AddDays(5)));
             var count = 1;
 
             // Quebra as entregas
--- BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore.Tests/UnitTest1.cs
@@ -11,12 +11,32 @@ namespace BaltaStore.Tests
         public void TestMethod1()
         {
             var name = new Name ( "Bruno", "Cunha" );
-            Document document = new Document("12345678911");
-            Email email = new Email("bruno@bruno.com");
+            var document = new Document("12345678911");
+            var email = new Email("bruno@bruno.com");
 
             var customer = new Customer( name, document, email, "4335420000");
 
             var order = new Order(customer);
+
+            var mouse = new Product("Mouse", "Rato", "mouse.png", 59.90M, 10);
+            var tecldo = new Product("Teclado", "Teclado", "mouse.png", 159.80M, 10);
+            var impressora = new Product("Impressora", "Impresssora", "mouse.png", 359.80M, 10);
+            var cadeira = new Product("Cadeira", "Cadeira", "mouse.png", 559.80M, 10);
+            
+            
+
+            order.AddItem( new OrderItem(mouse, 5));
+            order.AddItem( new OrderItem(tecldo, 5));
+            order.AddItem( new OrderItem(cadeira, 5));
+            order.AddItem( new OrderItem(impressora, 5));
+
+            order.Place();
+
+            order.Pay();
+
+            order.Ship();
+
+            order.Cancel();
             
         }
     }
