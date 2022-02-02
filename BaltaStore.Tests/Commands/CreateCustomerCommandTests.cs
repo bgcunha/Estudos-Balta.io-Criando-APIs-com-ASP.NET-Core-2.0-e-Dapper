ea8849640d4b96a044b8c45d52420dd598eaff24
Bruno Gomes <brunnu.cunha@gmail.com>
2022-02-02 11:58:15 -0300

    add teste de command

---
+++ BaltaStore.Tests/Commands/CreateCustomerCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Inputs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaltaStore.Tests.Commands
+{
+    [TestClass]
+    public class CreateCustomerCommandTests
+    {
+        [TestMethod]
+        public void ShouldValidateWhenCommandIsValid()
+        {
+            var command = new CreateCustomerCommand();
+            command.FirstName = "Bruno";
+            command.LastName = "Cunha";
+            command.Document = "28659170377";
+            command.Email = "bruno@gmail.com";
+            command.Phone = "11999999997";
+
+            Assert.AreEqual(true, command.Valid());
+        }
+    }
+}
